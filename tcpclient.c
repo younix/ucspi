e162e632fd6767e21d81318f1f3cdca513357036
Jan Klemkow <j.klemkow@wemelug.de>
2014-05-24 23:35:13 +0200

    remove a lot of useless code

--- tcpclient.c
+++ tcpclient.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Jan Klemkow <j.klemkow@wemelug.de>
+ * Copyright (c) 2013-2014 Jan Klemkow <j.klemkow@wemelug.de>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -33,7 +33,7 @@ char **environ;
 void
 usage(void)
 {
-	fprintf(stderr, "tcpclient [-4|6] HOST PORT PROGRAM [ARGS]\n");
+	fprintf(stderr, "tcpclient [-4|6] host port program [args]\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -45,6 +45,7 @@ main(int argc, char*argv[], char *envp[])
 	int save_errno;
 	int s;
 	int ch;
+	char *argv0 = argv[0];
 	const char *cause = NULL;
 	environ = envp;
 
@@ -102,75 +103,12 @@ main(int argc, char*argv[], char *envp[])
 	if (s == -1) goto err;
 	freeaddrinfo(res0);
 
-//TODO: set ucspi variables
-#	define PIPE_READ 0
-#	define PIPE_WRITE 1
-	int pi[2];
-	int po[2];
-	if (pipe(pi) < 0) goto err;
-	if (pipe(po) < 0) goto err;
+	if (dup2(s, 6) == -1) goto err;
+	if (dup2(s, 7) == -1) goto err;
+	if (close(s) == -1) goto err;
 
-	int prog_pid = fork();
-	switch (prog_pid) {
-	case 0:	/* start client program */
-
-		if (close(pi[PIPE_READ]) < 0) goto err;
-		if (close(po[PIPE_WRITE]) < 0) goto err;
-
-		/*
-		 * We have to move one descriptor cause po[] may overlaps with
-		 * descriptor 6 and 7.
-		 */
-		int po_read = 0;
-		if ((po_read = dup(po[PIPE_READ])) < 0) goto err;
-		if (close(po[PIPE_READ]) < 0) goto err;
-
-		if (dup2(pi[PIPE_WRITE], 6) < 0) goto err;
-		if (dup2(po_read, 7) < 0) goto err;
-
-		if (close(pi[PIPE_WRITE]) < 0) goto err;
-		if (close(po_read) < 0) goto err;
-		execve(prog, argv, environ);
-	case -1:
-		goto err;
-	}
-
-	if (close(pi[PIPE_WRITE]) < 0) goto err;
-	if (close(po[PIPE_READ]) < 0) goto err;
-
-	int rfd = pi[PIPE_READ];
-	int wfd = po[PIPE_WRITE];
-
-	char buf[BUFSIZ];
-	ssize_t len = 0;
-	fd_set readfds;
-	int max = 0;
-
-	for (;;) {
-		FD_ZERO(&readfds);
-		FD_SET(s, &readfds);
-		max = s;
-		FD_SET(rfd, &readfds);
-		if (max < rfd) max = rfd;
-
-		if (select(max+1, &readfds, NULL, NULL, NULL) < 0) goto err;
-
-		if (FD_ISSET(s, &readfds)) {
-			if ((len = recv(s, buf, sizeof buf, 0)) < 0) goto err;
-			if (len == 0) { /* connection was closed */
-				return EXIT_SUCCESS;
-			}
-			if ((write(wfd, buf, len)) < len) goto err;
-		}
-		if (FD_ISSET(rfd, &readfds)) {
-			if ((len = read(rfd, buf, sizeof buf)) < 0) goto err;
-			if (len == 0)
-				err(EXIT_FAILURE, "pipe closed\n");
-			if (send(s, buf, len, 0) < len) goto err;
-		}
-	}
-
+	execve(prog, argv, environ);
  err:
-	perror(cause);
+	perror(argv0);
 	return EXIT_FAILURE;
 }
