3bd6d82d9789ef6664df4ee7b4d2a034b54b1eea
Jakob Kramer <jakob.kramer@gmx.de>
2015-01-31 01:52:18 +0100

    use fd 6 to read and fd 7 to write

--- tlsc.c
+++ tlsc.c
@@ -58,8 +58,8 @@ main(int argc, char *argv[], char *envp[])
 	int out = STDOUT_FILENO;
 
 	/* pipes to communicate with the back end */
-	int sout = 6;
-	int sin = 7;
+	int sout = 7;
+	int sin = 6;
 
 	char *ca_file = NULL;
 	char *ca_path = NULL;
@@ -123,8 +123,8 @@ main(int argc, char *argv[], char *envp[])
 		if ((po_read = dup(po[PIPE_READ])) < 0) goto err;
 		if (close(po[PIPE_READ]) < 0) goto err;
 
-		if (dup2(pi[PIPE_WRITE], 6) < 0) goto err;
-		if (dup2(po_read, 7) < 0) goto err;
+		if (dup2(pi[PIPE_WRITE], 7) < 0) goto err;
+		if (dup2(po_read, 6) < 0) goto err;
 
 		if (close(pi[PIPE_WRITE]) < 0) goto err;
 		if (close(po_read) < 0) goto err;
