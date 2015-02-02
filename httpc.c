1269e92e89c6b3bc878b9d8997ff1c977ef27a9e
Jan Klemkow <j.klemkow@wemelug.de>
2015-02-02 08:35:22 +0100

    httpc: wip: add some code for header parsing

--- httpc.c
+++ httpc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Jan Klemkow <j.klemkow@wemelug.de>
+ * Copyright (c) 2014-2015 Jan Klemkow <j.klemkow@wemelug.de>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -26,18 +26,29 @@
 #define PIPE_WRITE 6
 #define PIPE_READ 7
 
+enum method {GET, HEAD};
+enum encoding {CHUNKED, GZIP};
+
+struct header {
+	size_t content_length;
+};
+
 static bool
-output(char *file)
+output(char *file, FILE *fh)
 {
-	ssize_t size;
 	int fd = STDOUT_FILENO;
+	size_t size;
 	char buf[BUFSIZ];
 
-	if (strcmp(file, "-") != 0)
+	fprintf(stderr, "output()\n");
+
+	if (strcmp(file, "-") != 0 && strcmp(file, "/"))
 		if ((fd = open(file, O_WRONLY)) == -1) return false;
 
-	while ((size = read(PIPE_READ, buf, sizeof buf)) > 0)
+	while ((size = fread(buf, sizeof buf, 1, fh)) > 0) {
+		fprintf(stderr, "output(): %zd\n", size);
 		if (write(fd, buf, size) == -1) return false;
+	}
 
 	if (size == -1)
 		return false;
@@ -46,16 +57,31 @@ output(char *file)
 	return true;
 }
 
+void
+parse_header_line(char *line, struct header *header)
+{
+	char *value = strchr(line, ':');
+	*value = '\0';
+	value++;
+
+	if (strcmp(line, "Content-Length") == 0) {
+		header->content_length = strtol(value, NULL, 0);
+	} else {
+		fprintf(stderr, "unknown: %s\n", line);
+	}
+}
+
 static bool
-read_response(FILE *fh)
+read_response(struct header *header)
 {
 	char buf[BUFSIZ];
 
-	//if (fgets(buf, sizeof buf, fh) == NULL) return false;
-
-	fprintf(stderr, "%s: read loop!\n", __func__);
-	while (fgets(buf, sizeof buf, fh) != NULL) {
-		fprintf(stderr, "blub%s\n", buf);
+	while (read(PIPE_READ, buf, sizeof buf) > 0) {
+		fprintf(stderr, "head: %s", buf);
+		char *eol = strstr(buf, "\r\n");
+		eol = '\0';
+		char *line = buf;
+		parse_header_line(buf, header);
 		if (strcmp(buf, "\r\n")) return true;
 	}
 
@@ -78,13 +104,9 @@ main(int argc, char *argv[])
 	char *uri = "/";
 	FILE *read_fh = NULL;
 	FILE *write_fh = NULL;
-	int i = 0;
 
 	if (setvbuf(stdout, NULL, _IONBF, 0) != 0)
 		err(EXIT_FAILURE, "setvbuf");
-
-	fprintf(stderr, "START httpc\n");
-	fprintf(stderr, "httpc i = %d\n", i++);
 
 	while ((ch = getopt(argc, argv, "H:o:h")) != -1) {
 		switch (ch) {
@@ -103,19 +125,15 @@ main(int argc, char *argv[])
 	argc -= optind;
 	argv += optind;
 
-	fprintf(stderr, "httpc i = %d\n", i++);
 	if (argc > 0)
 		uri = argv[0];
 
-	fprintf(stderr, "httpc i = %d - fdopen\n", i++);
 	if ((read_fh = fdopen(PIPE_READ, "r")) == NULL)
 		err(EXIT_FAILURE, "fopen");
 
-	fprintf(stderr, "httpc i = %d - fdopen\n", i++);
 	if ((write_fh = fdopen(PIPE_WRITE, "w")) == NULL)
 		err(EXIT_FAILURE, "fopen");
 
-	fprintf(stderr, "httpc i = %d - fprintf\n", i++);
 	/* print request */
 	fprintf(write_fh, "GET %s HTTP/1.1\r\n", uri);
 
@@ -124,15 +142,12 @@ main(int argc, char *argv[])
 
 	fprintf(write_fh, "\r\n");
 	fflush(write_fh);
-	fprintf(stderr, "httpc i = %d - read_response\n", i++);
 	/* get response */
-	read_response(read_fh);
+	read_response_header(read_fh);
 
-	fprintf(stderr, "httpc i = %d - read uri\n", i++);
-
-//	if (file == NULL)
-//		file = basename(uri);
-//	if (output(file) == false) goto err;
+	if (file == NULL)
+		file = basename(uri);
+	if (output(file, read_fh) == false) goto err;
 
 	return EXIT_SUCCESS;
  err:
