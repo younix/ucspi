3cde37172bb0bf14c82f50c68be846b8bf0c2222
Jan Klemkow <j.klemkow@wemelug.de>
2015-03-31 02:22:13 +0200

    test.sh: add more test cases

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 . ./tap-functions -u
 
-plan_tests 16
+plan_tests 32
 
 # prepare
 
@@ -16,16 +16,14 @@ file_grep() {
 
 tmpdir=$(mktemp -d tests_XXXXXX)
 
-#
-# tests starting here
-#
-
+#########################################################################
+# plain server to client communication					#
+#########################################################################
 CLIENT_PORT=$(($RANDOM % 65536 + 1024))
 SERVER_PORT=$(($RANDOM % 65536 + 1024))
 ./tcps 127.0.0.1 $SERVER_PORT /usr/bin/env &
 ./tcpc -p $CLIENT_PORT 127.0.0.1 $SERVER_PORT ./read.sh	\
 	1> $tmpdir/env.txt 				\
-	2> $tmpdir/err.txt
 
 ok $? "plaintext connection"
 
@@ -40,7 +38,32 @@ file_grep $tmpdir/env.txt "^TCPLOCALHOST=localhost\$"
 file_grep $tmpdir/env.txt "^TCPLOCALPORT=$SERVER_PORT\$"
 file_grep $tmpdir/env.txt "^PROTO=TCP\$"
 
-# ssl test
+#########################################################################
+# plain client to server communication				#
+#########################################################################
+CLIENT_PORT=$(($RANDOM % 65536 + 1024))
+SERVER_PORT=$(($RANDOM % 65536 + 1024))
+./tcps 127.0.0.1 $SERVER_PORT				\
+	./read.sh "0" "$tmpdir/env.txt" &
+./tcpc -p $CLIENT_PORT 127.0.0.1 $SERVER_PORT		\
+	./write.sh
+
+ok $? "tls connection client -> server"
+
+kill %1
+
+# client side environment
+file_grep $tmpdir/env.txt "^TCPREMOTEIP=127.0.0.1\$"
+file_grep $tmpdir/env.txt "^TCPREMOTEHOST=localhost\$"
+file_grep $tmpdir/env.txt "^TCPREMOTEPORT=$SERVER_PORT\$"
+file_grep $tmpdir/env.txt "^TCPLOCALIP=127.0.0.1\$"
+file_grep $tmpdir/env.txt "^TCPLOCALHOST=localhost\$"
+file_grep $tmpdir/env.txt "^TCPLOCALPORT=$CLIENT_PORT\$"
+file_grep $tmpdir/env.txt "^PROTO=TCP\$"
+
+#########################################################################
+# encrypted server to client communication				#
+#########################################################################
 CLIENT_PORT=$(($RANDOM % 65536 + 1024))
 SERVER_PORT=$(($RANDOM % 65536 + 1024))
 ./tcps 127.0.0.1 $SERVER_PORT				\
@@ -48,9 +71,9 @@ SERVER_PORT=$(($RANDOM % 65536 + 1024))
 	/usr/bin/env &
 ./tcpc -p $CLIENT_PORT 127.0.0.1 $SERVER_PORT		\
 	./tlsc -C					\
-	./read.sh 1> $tmpdir/env.txt 2> $tmpdir/err.txt
+	./read.sh 1> $tmpdir/env.txt
 
-ok $? "tls connection"
+ok $? "tls connection server -> client"
 
 kill %1
 
@@ -61,6 +84,31 @@ file_grep $tmpdir/env.txt "^TCPREMOTEPORT=$CLIENT_PORT\$"
 file_grep $tmpdir/env.txt "^TCPLOCALIP=127.0.0.1\$"
 file_grep $tmpdir/env.txt "^TCPLOCALHOST=localhost\$"
 file_grep $tmpdir/env.txt "^TCPLOCALPORT=$SERVER_PORT\$"
+file_grep $tmpdir/env.txt "^PROTO=SSL\$"
+
+#########################################################################
+# encrypted client to server communication				#
+#########################################################################
+CLIENT_PORT=$(($RANDOM % 65536 + 1024))
+SERVER_PORT=$(($RANDOM % 65536 + 1024))
+./tcps 127.0.0.1 $SERVER_PORT				\
+	./tlss -c crt.pem -k key.pem			\
+	./read.sh "0" "$tmpdir/env.txt" &
+./tcpc -p $CLIENT_PORT 127.0.0.1 $SERVER_PORT		\
+	./tlsc -C					\
+	./write.sh
+
+ok $? "tls connection client -> server"
+
+kill %1
+
+# client side environment
+file_grep $tmpdir/env.txt "^TCPREMOTEIP=127.0.0.1\$"
+file_grep $tmpdir/env.txt "^TCPREMOTEHOST=localhost\$"
+file_grep $tmpdir/env.txt "^TCPREMOTEPORT=$SERVER_PORT\$"
+file_grep $tmpdir/env.txt "^TCPLOCALIP=127.0.0.1\$"
+file_grep $tmpdir/env.txt "^TCPLOCALHOST=localhost\$"
+file_grep $tmpdir/env.txt "^TCPLOCALPORT=$CLIENT_PORT\$"
 file_grep $tmpdir/env.txt "^PROTO=SSL\$"
 
 # clean up
