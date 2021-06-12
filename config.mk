0c8ff5a2b45a191cf728c6bb938b6999f6896144
Jan Klemkow <j.klemkow@wemelug.de>
2021-06-12 22:34:18 +0200

    simplify make

--- config.mk
+++ config.mk
@@ -4,11 +4,9 @@ BINDIR	= ${DESTDIR}${PREFIX}/bin/
 MANDIR	= ${DESTDIR}${PREFIX}/man/
 MAN1DIR	= ${MANDIR}/man1/
 
-VERSION = 1.5
+VERSION = 1.7
 
 # compiler
-CC	= cc
-CFLAGS	= -std=c99 -pedantic -Wall -Wextra
+CFLAGS += -std=c99 -pedantic -Wall -Wextra
 CFLAGS += -D_XOPEN_SOURCE=700
 CFLAGS += -D_BSD_SOURCE
-LDFLAGS =
